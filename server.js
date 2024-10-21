11b2967d320cb5c6d6e11627b477b73436a7b63f
GiftinC <cgiftin@gmail.com>
2024-10-21 23:32:43 +0530

    Small Change

--- server.js
+++ server.js
@@ -18,13 +18,6 @@ const corsOptions = {
     credentials: true,
     optionsSuccessStatus: 204
 };
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://aquamarine-jalebi-01eea5.netlify.app"); // Adjust to your frontend's origin
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
   
 
 app.use(cors(corsOptions));
